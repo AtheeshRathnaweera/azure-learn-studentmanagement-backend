0db88b36f109612a59c465fb625bb207b2bd11b7
Atheesh Rathnaweera <rathnaweeraatheesh72@gmail.com>
2020-08-03 16:00:45 +0530

    intial commit

--- src/main/java/com/atheesh/studentmanagement/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atheesh.studentmanagement.repository;
-
-import com.atheesh.studentmanagement.Entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
